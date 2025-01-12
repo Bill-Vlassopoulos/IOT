6aea45968b97265bad1c46eae8d4cae1e794fd8b
Vassilis Vlassopoulos <42681317+Bill-Vlassopoulos@users.noreply.github.com>
2025-01-12 12:12:55 +0200

    working on admin

--- public/admin.mjs
+++ public/admin.mjs
@@ -1,42 +1,98 @@
-const dropdown = document.getElementById("dropdown");
-const dropdown1 = document.getElementById("dropdown1");
+//VARIABLES
+let currentJunctionId = null;
+var lastClickedMarker = null;
+var trafficLightMarkers = [];
+let trafficLights = [];
+let lastclickedtrafficlight = {};
 
-const fanaria = {
-    "v3_omada14_diastavrosi_0": ["v3_omada14_fanari_0", "v3_omada14_fanari_1", "v3_omada14_fanari_2", "v3_omada14_fanari_3"],
-    "v3_omada14_diastavrosi_1": ["v3_omada14_fanari_4", "v3_omada14_fanari_5", "v3_omada14_fanari_6", "v3_omada14_fanari_7"],
-    "v3_omada14_diastavrosi_2": ["v3_omada14_fanari_8", "v3_omada14_fanari_9", "v3_omada14_fanari_10", "v3_omada14_fanari_11"],
-    "v3_omada14_diastavrosi_3": ["v3_omada14_fanari_12", "v3_omada14_fanari_13", "v3_omada14_fanari_14", "v3_omada14_fanari_15"],
-    "v3_omada14_diastavrosi_4": ["v3_omada14_fanari_16", "v3_omada14_fanari_17", "v3_omada14_fanari_18", "v3_omada14_fanari_19"],
-}
+//ICONS
 
-function addOptions(selectElement, options) {
-    options.forEach(option => {
-        const opt = document.createElement('option');
-        opt.value = options.indexOf(option);
-        opt.textContent = option;
-        selectElement.appendChild(opt);
-    });
-}
+var defaultIcon = L.icon({
+    iconUrl: "pngwing.com.png",
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41],
+});
 
-function removeOptions(selectElement) {
-    while (selectElement.options.length > 0) {
-        selectElement.remove(0);
+var trafficLightIcon = L.icon({
+    iconUrl: "traffic-light.png",
+    iconSize: [25, 30], // Adjust the size as needed
+    iconAnchor: [7, 30],
+    popupAnchor: [4, -30],
+    shadowSize: [25, 25],
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    const map = L.map('map').setView([38.292488, 21.789119], 14); // Coordinates for Patras, Greece
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    fetchJunctions(map);
+    fetchTrafficLights(lastClickedMarker)
+});
+
+async function fetchJunctions(map) {
+    try {
+        const response = await fetch("/api/junctions");
+        const data = await response.json();
+        const locations = data.junctions;
+
+        locations.forEach(function (location, index) {
+            var marker = L.marker([location.lat, location.lng], {
+                icon: defaultIcon,
+            })
+                .addTo(map)
+                .bindPopup(location.title)
+                .on("click", function () {
+                    if (lastClickedMarker) {
+                        lastClickedMarker.setIcon(defaultIcon);
+                        trafficLightMarkers.forEach(function (tlMarker) {
+                            map.removeLayer(tlMarker);
+                        });
+                        trafficLightMarkers = [];
+                    }
+
+                    lastClickedMarker = marker;
+                    map.flyTo([location.lat, location.lng], 18);
+                    currentJunctionId = location.id;
+                    console.log("Current Junction ID:", currentJunctionId);
+                    //subscribeToTopic(`v3_fanaria/${currentJunctionId}`);
+                });
+        });
+    } catch (error) {
+        console.error("Error fetching junctions:", error);
     }
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-    addOptions(dropdown, Object.keys(fanaria));
-});
+async function fetchTrafficLights(location) {
+    try {
+        const response = await fetch(`/api/traffic-lights/${location}`);
+        const tlData = await response.json();
+        trafficLights = tlData.trafficLights;
 
+        trafficLights.forEach(function (tl) {
+            var tlMarker = L.marker([tl.lat, tl.lng], {
+                icon: trafficLightIcon,
+            })
+                .addTo(map)
+                .bindPopup(` ${tl.title}`)
+                .on("click", function () {
+                    lastclickedtrafficlight = {
+                        junction: location.id,
+                        trafficLight: tl.id,
+                    };
 
-dropdown.addEventListener('change', (e) => {
-    const selected = e.target.options[e.target.selectedIndex].text;
-    console.log(selected);
-    console.log(fanaria[selected]);
-    removeOptions(dropdown1);
-    addOptions(dropdown1, fanaria[selected]);
-});
+                    currentTrafficLightId = tl.id;
+                    console.log("Current Traffic Light ID:", currentTrafficLightId);
+                });
 
+            trafficLightMarkers.push(tlMarker);
+        });
 
-
-
+    } catch (error) {
+        console.error("Error fetching traffic lights: ", error);
+    }
+}
